b05bef244abcefd870b73bb5f46cb23bdef33ff1
Martin Fogliacco <fogliaccomartin3@gmail.com>
2024-03-18 21:18:32 -0300

    remove: i18n library deleted

--- src/utils/i18n.ts
+++
@@ -1,8 +0,0 @@
-type Translations = {
-    greeting: string;
-};
-
-export async function loadTranslations(locale: string): Promise<Translations> {
-    const translations = await import(`../content/${locale}/${locale}.json`);
-    return translations.default;
-}
