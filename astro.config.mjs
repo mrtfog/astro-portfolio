ac0aa6d09c81809b5d82dc926109aae5e918d322
Martin Fogliacco <fogliaccomartin3@gmail.com>
2024-03-23 03:55:05 -0300

    add: astro.config.mjs translations import

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,4 +5,11 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
     integrations: [tailwind(), react()],
+    i18n: {
+        defaultLocale: "en",
+        locales: ["en", "es"],
+        rounting: {
+            prefixDefaultLocale: false,
+        },
+    },
 });
