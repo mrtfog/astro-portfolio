709f53d5610a9c882b6d76b0896097ae6346e1b5
Martin Fogliacco <fogliaccomartin3@gmail.com>
2024-03-23 03:54:17 -0300

    chore: Necessary libraries added, absolutes paths added, & font imports at tailwind.config

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+    content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+    theme: {
+        extend: {
+            fontFamily: {
+                raleway: ["Raleway Variable", "sans-serif"],
+                rubik: ["Rubik Variable", "sans-serif"],
+            },
+        },
+    },
+    plugins: [],
+};
