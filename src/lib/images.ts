6f3c5371613f438e64a1468531feb4927c61d9b4
Martin Fogliacco <fogliaccomartin3@gmail.com>
2024-03-23 03:51:59 -0300

    add: Project Images

---
+++ src/lib/images.ts
@@ -0,0 +1,6 @@
+import AboutImage from "@/images/about-image.webp";
+import AristotelesImage from "@/images/aristoteles-image.webp";
+import ContactTitleImage from "@/images/contact-title-image.webp";
+import HeroTitleImage from "@/images/hero-image.webp";
+
+export { AboutImage, AristotelesImage, ContactTitleImage, HeroTitleImage };
